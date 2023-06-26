0e7b438c8c36cf8c0fa85ae584dbb056e87fdd43
JaeDoo <fosong98@gmail.com>
2023-06-26 15:05:28 +0900

    feat : 가게 서비스 초기 설정

---
+++ src/main/java/com/c9pay/storeservice/StoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.c9pay.storeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreServiceApplication.class, args);
+    }
+
+}
