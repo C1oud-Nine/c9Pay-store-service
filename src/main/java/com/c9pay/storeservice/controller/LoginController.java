60b5fafefbf40b28331c08919f43dbfb659721a8
JaeDoo <fosong98@gmail.com>
2023-06-26 21:32:02 +0900

    feat : default login controller 작성

---
+++ src/main/java/com/c9pay/storeservice/controller/LoginController.java
@@ -0,0 +1,26 @@
+package com.c9pay.storeservice.controller;
+
+import com.c9pay.storeservice.dto.StoreDetails;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/{store-id}")
+public class LoginController {
+    @PostMapping("/login")
+    public ResponseEntity<StoreDetails> login(@PathVariable("store-id") int storeId, HttpServletResponse response) {
+        response.addCookie(new Cookie("Authorization", "dummy-store-token"));
+
+        // todo 사용자 식별번호 획득
+        // todo store id 검증 로직
+
+        return ResponseEntity.ok(new StoreDetails((long) storeId, "store1"));
+    }
+}
