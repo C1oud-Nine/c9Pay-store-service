bf9f0bc22a800c71841608adf06e1be7d0bce9d6
JaeDoo <fosong98@gmail.com>
2023-07-02 18:14:51 +0900

    feat: 가게 생성 및 조회 로직 작성, 테스트 작성

---
+++ src/main/java/com/c9pay/storeservice/repository/StoreRepository.java
@@ -0,0 +1,11 @@
+package com.c9pay.storeservice.repository;
+
+import com.c9pay.storeservice.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface StoreRepository extends JpaRepository<Store, UUID> {
+    public List<Store> findAllByUserId(UUID userId);
+}
---
+++ src/main/java/com/c9pay/storeservice/service/StoreService.java
@@ -0,0 +1,26 @@
+package com.c9pay.storeservice.service;
+
+import com.c9pay.storeservice.dto.store.StoreDetails;
+import com.c9pay.storeservice.entity.Store;
+import com.c9pay.storeservice.repository.StoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+    private final StoreRepository storeRepository;
+
+    public Store createStore(UUID storeId, UUID userId, String name) {
+        return storeRepository.save(new Store(name, storeId, userId));
+    }
+
+    public List<StoreDetails> getAllStoreDetails(UUID userId) {
+        return storeRepository.findAllByUserId(userId).stream()
+                .map((s)->new StoreDetails(s.getId(), s.getName()))
+                .toList();
+    }
+}
