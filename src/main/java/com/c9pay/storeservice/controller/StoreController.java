5b56cba4c6485c2a0dc61e1916b51e4d6e99dfa8
fosong98 <fosong98@gmail.com>
2023-07-06 16:32:41 +0900

    feat: 가게 등록 및 조회 로직 작성

--- src/main/java/com/c9pay/storeservice/controller/StoreController.java
+++ src/main/java/com/c9pay/storeservice/controller/StoreController.java
@@ -19,23 +19,31 @@ import java.util.UUID;
 @RequestMapping("/store")
 public class StoreController {
     private final StoreService storeService;
-    @GetMapping
-    public ResponseEntity<StoreDetailList> getStores() {
-        // todo Store 목록 조회 작성
-        List<StoreDetails> storeDetailsList = List.of(
-                new StoreDetails(1L, "store1"),
-                new StoreDetails(2L, "store2"),
-                new StoreDetails(3L, "store3")
-        );
 
+    /**
+     * 사용자 토큰을 받아서 사용자 서비스를 통해 식별번호로 변경한 후,
+     * 사용자가 보유한 모든 가게를 응답한다.
+     *
+     * @param userId 사용자 서비스를 통해 획득한 사용자 식별번호
+     * @return 사용자의 모든 가게를 응답
+     */
+    @GetMapping
+    public ResponseEntity<StoreDetailList> getStores(@RequestAttribute UUID userId) {
+        List<StoreDetails> storeDetailsList = storeService.getAllStoreDetails(userId);
         return ResponseEntity.ok(new StoreDetailList(storeDetailsList));
     }
 
+    /**
+     * 사용자 토큰을 받아서 사용자 서비스를 통해 식별번호로 변경한 후,
+     * 획득한 가게 정보를 토대로 가게를 만든다.
+     * 그 후, 사용자가 보유한 모든 가게를 조회하여 응답한다.
+     *
+     * @param userId 사용자 서비스를 통해 획득한 사용자 식별번호
+     * @param storeForm 가게를 생성하기 위한 가게 정보
+     * @return 사용자의 모든 가게를 응답
+     */
     @PostMapping
-    public ResponseEntity<StoreDetailList> addStores(@RequestBody StoreForm storeForm) {
-        // todo filter단에서 사용자 토큰을 식별번호로 변경
-        UUID userId = storeForm.getUserId();
-
+    public ResponseEntity<StoreDetailList> addStores(@RequestAttribute UUID userId, @RequestBody StoreForm storeForm) {
         // todo 가게 식별번호를 획득하는 로직 작성
         UUID storeId = UUID.randomUUID();
         Store store = storeService.createStore(storeId, userId, storeForm.getName());
